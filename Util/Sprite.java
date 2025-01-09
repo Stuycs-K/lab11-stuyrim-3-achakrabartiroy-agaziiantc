0100898b2501d28a2be0251855516bf9542ee963
bread1337j <131266168+bread1337j@users.noreply.github.com>
2025-01-09 09:41:16 -0500

    Made sprite class and spriteSheet to hold the sprites

---
+++ Util/Sprite.java
@@ -0,0 +1,8 @@
+public class Sprite {
+	public int[][] map;
+	public String[] texture;
+	public Sprite(String[] s, int[][] n){
+		this.map = n;
+		this.texture = s;
+	}
+}
---
+++ Util/spriteSheet.java
@@ -0,0 +1,20 @@
+public class spriteSheet {
+	
+	public static final testSprite = new Sprite(
+		new int[][] {
+			{0, 0},
+			{0, 1},
+			{1, 0},
+			{1, 1}
+		},
+
+		 new String[]{
+		 	"a", "b", "c", "d"
+		 }
+		 
+	);
+
+
+
+
+}
